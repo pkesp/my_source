60559e633de1b8b35de351e26939fa9715b190d2
pkesp <51509101+pkesp@users.noreply.github.com>
2020-02-28 13:37:19 +0900

    글라이더 입력과 움직임

--- CSharp/ControllerInput.cs
+++ CSharp/ControllerInput.cs
@@ -2,11 +2,12 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
+
 
 public class ControllerInput : MonoBehaviour
 {
-    public RectTransform Stick;
-    public Transform plane;
+    private RectTransform Stick;
 
     private Vector2 stickFirstPos;
     private Vector3 joyVec;
@@ -25,6 +26,20 @@ public class ControllerInput : MonoBehaviour
     float yawAxisDelta;
 
     private TerrainCollision _terrainColli;
+
+    //
+    // James-20.0114 added to use Zyrosensor value.
+    //
+    private bool m_bUseZyroSensor = true;  // true(zyro sensor 사용), false(sensor 외 사용: 예: 조이스틱)
+    private Communicator clsCommu;
+    private bool m_bFirstRead = true;
+
+    private float pitchAxisDelta_First; // 변화량 계산을 위함
+    private float rollAxisDelta_First;
+
+    private float m_fDeltaPitch;
+    private float m_fDeltaRoll;
+
 
     void Awake()
     {
@@ -46,56 +61,113 @@ public class ControllerInput : MonoBehaviour
         }
 
         maxAngle = 50f;
+
+        // James-20.0114 added. -> JMH-20.0203 added.
+        clsCommu = GameObject.FindGameObjectWithTag("Communicator").GetComponent<Communicator>();
     }
 
     // Start is called before the first frame update
     void Start()
     {
-        Radius = GetComponent<RectTransform>().sizeDelta.y * 0.5f;
-        stickFirstPos = Stick.anchoredPosition;
-        _terrainColli = GameObject.Find("ParaGlider2").GetComponent<TerrainCollision>();
-        
+        //if (SceneManager.GetActiveScene().buildIndex == 1)
+        //{
+        //    Radius = GameObject.Find("Canvas (1)").transform.Find("background").GetComponent<RectTransform>().sizeDelta.y * 0.5f;
+        //    Stick = GameObject.Find("Canvas (1)").transform.Find("Stick").GetComponent<RectTransform>();
+        //    stickFirstPos = Stick.anchoredPosition;
+        //}
+        _terrainColli = GameObject.FindGameObjectWithTag("Glider").GetComponent<TerrainCollision>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        ;
     }
 
     void FixedUpdate()
     {
-        if (!_terrainColli.GetIsStanding())
-        {
-            if (Mathf.Abs(Input.GetAxis("Pitch")) > 0.01f ||
-                Mathf.Abs(Input.GetAxis("Roll")) > 0.01f)
+        if (m_bUseZyroSensor)
+        {            
+            //////////////// tested value //////////////////////////
+            //
+            // 처음 값을 기준으로 변화량에 따른 동작을 수행한다.
+            //
+            if (m_bFirstRead)
             {
-                pitchAxis_origin = Input.GetAxis("Pitch");
-                rollAxis_origin = Input.GetAxis("Roll");
+                pitchAxisDelta_First = clsCommu.m_stZyroInfo.pitch;
+                rollAxisDelta_First = clsCommu.m_stZyroInfo.roll;
 
-                //Debug.Log("RollAxis : " + rollAxis_origin);
+                if (pitchAxisDelta_First != 0.0f && rollAxisDelta_First != 0.0f)
+                {
+                    m_bFirstRead = false;   // 더 이상 초기 값 설정하지 않기 위한 변수 변경
+                }
 
-                rollAxis = rollAxis_origin * Radius;
-                pitchAxis = pitchAxis_origin * Radius;
-                Stick.anchoredPosition = new Vector2(rollAxis, -pitchAxis) + stickFirstPos;
-
-                rollAxisDelta += rollAxis_origin * maxAngle * Time.deltaTime;
-                pitchAxisDelta += pitchAxis_origin * maxAngle * Time.deltaTime;
-
-                yawAxisDelta += rollAxisDelta * Time.deltaTime;
-
-                //plane.transform.eulerAngles = new Vector3(-pitchAxisDelta, 0, -rollAxisDelta);
+                // 처음에 받은 값으로 셋팅.
+                pitchAxisDelta = 0; //  clsCommu.m_stZyroInfo.pitch;
+                rollAxisDelta = 0; // clsCommu.m_stZyroInfo.roll;
+                yawAxisDelta = this.transform.eulerAngles.y; // rollAxisDelta;
             }
 
-            //if (Mathf.Abs(Input.GetAxis("Yaw")) > 0.05f)
-            //{
-            //    yawAxis_origin = Input.GetAxis("Yaw");
-            //    yawAxisDelta += yawAxis_origin * 15 * Time.deltaTime;
+            m_fDeltaPitch = clsCommu.m_stZyroInfo.pitch - pitchAxisDelta_First;
+            m_fDeltaRoll = clsCommu.m_stZyroInfo.roll - rollAxisDelta_First;
 
-            //    //plane.transform.eulerAngles = new Vector3(0, yawAxisDelta, 0);
-            //}
+            if (Mathf.Abs(m_fDeltaPitch) > 0.0f || Mathf.Abs(m_fDeltaRoll) > 0.0f)
+            {
+                pitchAxisDelta = (m_fDeltaPitch );
+                rollAxisDelta = (m_fDeltaRoll );
+                yawAxisDelta += (rollAxisDelta * Time.deltaTime);
+            }
 
-            // DLL 사용으로 잠시 묶어둠.
-            //plane.eulerAngles = new Vector3(-pitchAxisDelta, yawAxisDelta, -rollAxisDelta);
+            //this.transform.eulerAngles = new Vector3(pitchAxisDelta, -yawAxisDelta, rollAxisDelta);
+            this.transform.rotation = Quaternion.Euler(new Vector3(pitchAxisDelta, -yawAxisDelta, rollAxisDelta));
+
+        }
+        else // if (m_bUseZyroSensor == false)
+        {
+            #region 0 Joystic 사용시. original source 
+            if (!_terrainColli.GetIsStanding())
+            {
+                if (Mathf.Abs(Input.GetAxis("Pitch")) > 0.01f ||
+                Mathf.Abs(Input.GetAxis("Roll")) > 0.01f)
+                {
+                    pitchAxis_origin = Input.GetAxis("Pitch");
+                    rollAxis_origin = Input.GetAxis("Roll");
+
+                    //Debug.Log("RollAxis : " + rollAxis_origin);
+
+                    rollAxis = rollAxis_origin * Radius;    // James-20.0114 added comment: Radious는 항상 '0' 임.. ?? 
+                    pitchAxis = pitchAxis_origin * Radius;
+
+                    //System.String strMsg = "Roll(org: " + rollAxis_origin + ", axis:" + rollAxis + ")" + "Pitch(org: " + pitchAxis_origin + ", axis:" + pitchAxis + ")";
+                    //Debug.Log(strMsg);
+
+                    if (SceneManager.GetActiveScene().name == "Competition_Scene_00")
+                    {
+                        Stick.anchoredPosition = new Vector2(rollAxis, -pitchAxis) + stickFirstPos; 
+                    }
+
+                    rollAxisDelta += rollAxis_origin * maxAngle * Time.deltaTime;
+                    pitchAxisDelta += pitchAxis_origin * maxAngle * Time.deltaTime;
+
+                    yawAxisDelta += rollAxisDelta * Time.deltaTime;
+
+                    //plane.transform.eulerAngles = new Vector3(-pitchAxisDelta, 0, -rollAxisDelta);
+                }
+
+                //if (Mathf.Abs(Input.GetAxis("Yaw")) > 0.05f)
+                //{
+                //    yawAxis_origin = Input.GetAxis("Yaw");
+                //    yawAxisDelta += yawAxis_origin * 15 * Time.deltaTime;
+
+                //    //plane.transform.eulerAngles = new Vector3(0, yawAxisDelta, 0);
+                //}
+
+                this.transform.eulerAngles = new Vector3(-pitchAxisDelta, yawAxisDelta, -rollAxisDelta);
+
+                System.String strMsg = "eulerAngles(" + this.transform.eulerAngles.x + "," + this.transform.eulerAngles.y + "," + this.transform.eulerAngles.z + ")";
+                Debug.Log(strMsg + "\n");
+            }
+            #endregion
         }
     }
 }
--- CSharp/GliderMove2.cs
+++ CSharp/GliderMove2.cs
@@ -1,10 +1,11 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GliderMove2 : MonoBehaviour
 {
-    public WindZone _wind;
+    private WindZone _wind;
 
     private float _spd;             // 행글라이더 평균 속도가 35 ~ 60km 라고 함.
     private float _accel;           // 가속도는 -35 ~ 60 사이
@@ -12,25 +13,34 @@ public class GliderMove2 : MonoBehaviour
     private float _maxAccel;        // 행글라이더 최고 속도가 급강하 할때 120km 까지 나온다고 함.
     private Rigidbody _rigid;
 
-    Vector3 _pos;
-
     Vector3 windVec;
     Vector3 spdVec;
     Vector3 rigidVec;
 
     TerrainCollision _terrColl;
+    RisingWind _rising;
+    InputSignal _inputSignal;
+
+    //
+    // James-20.0114 added to use Zyrosensor value.
+    //
+    private bool m_bUseZyroSensor = true;  // true(zyro sensor 사용), false(sensor 외 사용: 예: 조이스틱)
+    private Communicator clsCommu;
 
     bool _isStanding;
 
     void Awake()
     {
         _isStanding = false;
-        _spd = 35f;         
+        _spd = 35f;    
         _accel = 0f;
-        _maxAccel = 60f;    
-        _maxSpd = 120f;     
+        _maxAccel = 60f;
+        _maxSpd = 120f;
+        
+        // James-20.0114 added.
+        clsCommu = GameObject.FindGameObjectWithTag("Communicator").GetComponent<Communicator>();
 
-        _pos = this.transform.position;
+        _wind = GameObject.FindGameObjectWithTag("WindZone").GetComponent<WindZone>();
     }
 
     // Start is called before the first frame update
@@ -39,38 +49,74 @@ public class GliderMove2 : MonoBehaviour
         _terrColl = this.GetComponent<TerrainCollision>();
         _terrColl.Init();
         _rigid = this.GetComponent<Rigidbody>();
+        _rising = this.GetComponent<RisingWind>();
+        _inputSignal = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<InputSignal>();
+
+        if (SceneManager.GetActiveScene().buildIndex == 0)
+        {
+            _rigid.useGravity = true;
+        }
+        else if (SceneManager.GetActiveScene().buildIndex == 1)
+        {
+            _rigid.useGravity = true;
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
+        
+    }
+
+    void FixedUpdate()
+    {
         if (!_isStanding)
         {
-            windVec = _wind.transform.forward * (_wind.windMain / 3f);
+            if (m_bUseZyroSensor == true)
+            {
+                windVec = _wind.transform.forward * (_wind.windMain * 0.5f);
 
-            if (this.transform.localEulerAngles.x > 42f) _spd = 90f * 0.277778f;
-            else if (this.transform.localEulerAngles.x > 38f) _spd = 85f * 0.277778f;
-            else if (this.transform.localEulerAngles.x > 34f) _spd = 80f * 0.277778f;
-            else if (this.transform.localEulerAngles.x > 30f) _spd = 75f * 0.277778f;
-            else if (this.transform.localEulerAngles.x > 26f) _spd = 70f * 0.277778f;
-            else if (this.transform.localEulerAngles.x > 22f) _spd = 65f * 0.277778f;
-            else if (this.transform.localEulerAngles.x > 18f) _spd = 60f * 0.277778f;
-            else if (this.transform.localEulerAngles.x > 14f) _spd = 55f * 0.277778f;
-            else if (this.transform.localEulerAngles.x > 10f) _spd = 50f * 0.277778f;
-            else if (this.transform.localEulerAngles.x > 6f) _spd = 45f * 0.277778f;
-            else if (this.transform.localEulerAngles.x > 2f) _spd = 40f * 0.277778f;
-            else if (this.transform.localEulerAngles.x > -2f) _spd = 35f * 0.277778f;
-            else if (this.transform.localEulerAngles.x > -6f) _spd = 30f * 0.277778f;
-            else if (this.transform.localEulerAngles.x > -10f) _spd = 20f * 0.277778f;
-            else if (this.transform.localEulerAngles.x > -14f) _spd = 10f * 0.277778f;
-            else _spd = 5f * 0.277778f;
+                if (this.transform.localEulerAngles.x < 90f && this.transform.localEulerAngles.x >= 0f)
+                {
+                    _spd = 35f + (this.transform.localEulerAngles.x * 1.2f);
+                }
+                else if (this.transform.localEulerAngles.x < 360f && this.transform.localEulerAngles.x > 270f)
+                {
+                    _spd = 35f + ((this.transform.localEulerAngles.x - 360f) * 0.7f);
+                    _spd = Mathf.Clamp(_spd, 0, 35f);
+                }
 
-            spdVec = this.transform.forward * _spd;
-            //this.transform.position = this.transform.position + spdVec;
-            rigidVec = (spdVec + windVec); //* Time.deltaTime;
-            //Debug.Log(rigidVec);
+                spdVec = this.transform.forward * _spd;
+                rigidVec = (spdVec + windVec) * Time.deltaTime;
+            }
+            else // ########## Use the Joystick ##########
+            {
+                windVec = _wind.transform.forward * (_wind.windMain * 0.5f);
+
+                if (this.transform.localEulerAngles.x < 90f && this.transform.localEulerAngles.x >= 0f)
+                {
+                    //Debug.Log("this.transform.localEulerAngles.x : " + this.transform.localEulerAngles.x);
+                    _spd = 35f + (this.transform.localEulerAngles.x * 1.2f);
+                    //Debug.Log("_spd : " + _spd);
+                }
+                else if (this.transform.localEulerAngles.x < 360f && this.transform.localEulerAngles.x > 270f)
+                {
+                    _spd = 35f + ((this.transform.localEulerAngles.x - 360f) * 0.7f);
+                    _spd = Mathf.Clamp(_spd, 0, 35f);
+                }
+
+                spdVec = this.transform.forward * _spd;
+                rigidVec = (spdVec + windVec) * Time.deltaTime;
+
+
+                // James-20.0114 added.
+                //System.String strMsg = "Roll2: " + this.transform.localEulerAngles.z + "Pitch2: " + this.transform.localEulerAngles.x + "Yaw2: " + this.transform.localEulerAngles.y + "\n";
+                //Debug.Log(strMsg);
+            }
+
+
         }
-        
+
         _terrColl.TerrainDetection();
 
         if (_terrColl.GetIsStanding())
@@ -80,14 +126,24 @@ public class GliderMove2 : MonoBehaviour
         }
 
         if (!_isStanding)
-        {
-            _rigid.AddForce(rigidVec, ForceMode.Acceleration);
-            //_rigid.velocity = rigidVec;
+        {            
+            if (SceneManager.GetActiveScene().buildIndex == 0)
+            {
+                if (_inputSignal.GetIsMoveStart())
+                {
+                    _rigid.MovePosition(this.transform.position + rigidVec);
+                }
+            }
+            else
+            {
+                _rigid.MovePosition(this.transform.position + rigidVec);
+            }
         }
-    }
 
-    void FixedUpdate()
-    {
-        
+        if (_rising.GetIsRising())
+        {
+            _rigid.AddForce(Vector3.up * 2, ForceMode.Impulse);
+            _rising.SetIsRising(false);
+        }
     }
 }
